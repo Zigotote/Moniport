04ec190c0da05148564ce0973b162fc67ed3d912
Raphael Hascoet <hascoetraphael-29@yahoo.fr>
2019-10-12 16:39:03 +0200

    Measure id + timestamp, redis exists + incr + getInt

--- cmd/recepteur/recepteur.go
+++ cmd/recepteur/recepteur.go
@@ -1,11 +1,52 @@
 package main
 
 import (
+	"fmt"
 	redis "moniport/cmd/recepteur/redis"
+	"time"
 )
 
+type measure struct {
+	idSensor    string
+	idAirport   string
+	measureType string
+	value       float64
+	date        string
+}
+
 func main() {
-
+	redis.Connect()
 	redis.SendData("test", "yes")
+	for i := 0; i <= 10; i++ {
+		fmt.Println(getNewIdMeasure())
+	}
+	defer redis.CloseConnection()
+}
 
+func sendMeasure(m measure) {
+	setKey := m.idAirport + ":" + m.measureType
+
+	fmt.Println(getTimestampFromDate(m.date))
+}
+
+func getNewIdMeasure() int {
+	if redis.KeyExists("currIdMeasure") {
+		redis.IncrKey("currIdMeasure")
+	} else {
+		redis.SendData("currIdMeasure", "0")
+	}
+
+	return redis.GetDataInt("currIdMeasure")
+}
+
+func getTimestampFromDate(date string) int {
+	layout := "2006-01-02-15-04-05"
+
+	t, err := time.Parse(layout, date)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return t.Unix()
 }
--- cmd/recepteur/redis/redis.go
+++ cmd/recepteur/redis/redis.go
@@ -6,24 +6,68 @@ import (
 	redigo "github.com/gomodule/redigo/redis"
 )
 
+var conn redigo.Conn = nil
+
 func getConnection() redigo.Conn {
+	return conn
+}
+
+func Connect() {
 	c, err := redigo.Dial("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
 	}
-	return c
+	conn = c
 }
 
-func closeConnection(c redigo.Conn) {
-	c.Close()
+func CloseConnection() {
+	conn.Close()
 }
 
 func SendData(key string, data string) {
 	c := getConnection()
-	reply, err := c.Do("SET", key, data)
+	_, err := c.Do("SET", key, data)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Set " + key + ": " + data)
+	}
+}
+
+func getData(key string) (interface{}, error) {
+	c := getConnection()
+	reply, err := c.Do("GET", key)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(reply)
-	defer closeConnection(c)
+	return reply, err
+}
+
+func GetDataString(key string) string {
+	s, _ := redigo.String(getData(key))
+	return s
+}
+
+func GetDataInt(key string) int {
+	i, _ := redigo.Int(getData(key))
+	return i
+}
+
+func KeyExists(key string) bool {
+	c := getConnection()
+	reply, err := redigo.Int(c.Do("EXISTS", key))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return reply == 1
+}
+
+func IncrKey(key string) {
+	c := getConnection()
+	_, err := c.Do("INCR", key)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Incr " + key)
+	}
 }
