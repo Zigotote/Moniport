feb71684121df75262d7030c267b4b90628a9cc3
Zigotote <oce88b@gmail.com>
2019-10-17 21:58:16 +0200

    Avancement bash

--- cmd/sensor/sensor.sh
+++ cmd/sensor/sensor.sh
@@ -1,5 +1,12 @@
-cd $GOPATH/src/Moniport/cmd
+cd $GOPATH/src/Moniport/cmd/sensor
 go build Moniport/cmd/sensor
+
+if [ ! $? -eq 0 ]
+then
+    echo "Erreur lors du build du dossier sensor"
+fi
+
+cd ..
 for config in config-files/*.json 
 do
     ./sensor/sensor -config $GOPATH/src/Moniport/cmd/$config  &
