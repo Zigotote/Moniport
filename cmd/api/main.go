7948f22a5217d700b72a4217b86e436dfbc0c1cf
Zigotote <oce88b@gmail.com>
2019-10-23 20:37:47 +0200

    Début intégration redis

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,73 +1,73 @@
 package main
 
 import (
+	"Moniport/internal/data"
+	"Moniport/internal/helpers/date"
+	"Moniport/internal/helpers/redis"
+	"Moniport/internal/measuresrtrv"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	redis.Connect()
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/measures/{start}/{end}", measureHandler)
-	router.HandleFunc("/avg-measures/{date}", avgMeasureHandler)
+	router.HandleFunc("/measures/{airport}/{measure}/{start}/{end}", measureHandler)
+	router.HandleFunc("/avg-measures/{airport}/{date}", avgMeasureHandler)
 	err := http.ListenAndServe(":8081", router)
 	log.Fatal(err)
+	defer redis.CloseConnection()
 }
 
 func measureHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	start := getTimestampFromDate(vars["start"])
-	end := getTimestampFromDate(vars["end"])
-	if start < 0 || end < 0 {
+	airport := vars["airport"]
+	measure := vars["measure"]
+	start := date.ParseDate(vars["start"])
+	end := date.ParseDate(vars["end"])
+	if start.Year() == 1 || end.Year() == 1 {
 		fmt.Fprint(w, "Vous devez renseigner les paramètres start et end pour effectuer la requête. Ils doivent être au format YYYY-MM-DD-hh-mm-ss.")
 		return
 	}
-	//appel redis getMeasure(start, end)
-	resp := Message{
-		Date:        "1571351477",
-		MeasureType: "temp",
-		Value:       12,
-		IDSensor:    "1",
-		IDAirport:   "NTE",
-	}
-	//voir pour convertir les timestamp en dates
+	resp := measuresrtrv.GetMeasures(airport, measure)
 	writeJSON(w, resp)
 }
 
 func avgMeasureHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	date := getTimestampFromDateDay(vars["date"])
-	if date < 0 {
+	date := date.ParseDate(vars["date"])
+	/*if date < 0 {
 		fmt.Fprint(w, "Vous devez renseigner le paramètre date pour effectuer la requête. Il doit être au format YYYY-MM-DD")
 		return
-	}
+	}*/
+	date.AddDate(1, 1, 1)
 	//appel redis getAvgMeasures(date)
-	temp := Message{
+	temp := data.Measure{
 		Date:        "1571351477",
 		MeasureType: "temp",
 		Value:       12,
 		IDSensor:    "1",
 		IDAirport:   "NTE",
 	}
-	wind := Message{
+	wind := data.Measure{
 		Date:        "1571351477",
 		MeasureType: "wind",
 		Value:       80,
 		IDSensor:    "2",
 		IDAirport:   "NTE",
 	}
-	press := Message{
+	press := data.Measure{
 		Date:        "1571351477",
 		MeasureType: "press",
 		Value:       1200,
 		IDSensor:    "3",
 		IDAirport:   "NTE",
 	}
-	resp := []Message{temp, wind, press}
+	resp := []data.Measure{temp, wind, press}
 	//voir pour convertir les timestamp en dates
 	writeJSON(w, resp)
 }
@@ -80,39 +80,4 @@ func writeJSON(w http.ResponseWriter, data interface{}) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonData)
-}
-
-//Type temporaire en attendant que le refacto soit ok
-type Message struct {
-	IDSensor    string  `json:"idSensor"`
-	IDAirport   string  `json:"idAirport"`
-	MeasureType string  `json:"measure"`
-	Value       float32 `json:"value"`
-	Date        string  `json:"date"`
-}
-
-//fonction reprise de recepteur.go, à refactorer
-func getTimestampFromDate(date string) int64 {
-	layout := "2006-01-02-15-04-05"
-
-	t, err := time.Parse(layout, date)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return t.Unix()
-}
-
-//à mettre dans le même fichier que la méthode getTimestampFromDate après refactoring
-func getTimestampFromDateDay(date string) int64 {
-	layout := "2006-01-02"
-
-	t, err := time.Parse(layout, date)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return t.Unix()
 }
