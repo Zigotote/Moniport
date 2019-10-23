f606a53ca843f4a43d2dbe15eac629dfefc23585
lea <lea.louesdon@gmail.com>
2019-10-23 22:02:32 +0200

    Liked pages

--- cmd/templating/main.go
+++ cmd/templating/main.go
@@ -1,38 +1,43 @@
 package main
 
 import (
+	data "Moniport/internal/data"
+	redis "Moniport/internal/helpers/redis"
+	measuresrtrv "Moniport/internal/measuresrtrv"
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
-type entry struct {
-	Name string
-	Done bool
-}
-
-type person struct {
-	UserName string
-}
-
-type Aeroport struct {
-	Name string
-	id   string
-}
-type datas struct {
-	Aeroports []Aeroport
-}
-
 func templateHandeler(w http.ResponseWriter, r *http.Request) {
-	data := datas{Aeroports: []Aeroport{
-		{Name: "Nantes", id: "NAN"},
-		{Name: "Londres", id: "LND"},
-	}}
-	t := template.Must(template.ParseFiles("tmpl/pageAcceuil.html", "tmpl/css/theme.css"))
-	fmt.Println(t.Execute(w, data))
+	/*datas := data.Datas{Aeroports: []data.Aeroport{
+		{Name: "Nantes", Id: "NAN"},
+		{Name: "Londres", Id: "LND"},
+	}}*/
+	datas := data.Datas{Aeroports: []data.Aeroport{}}
+	airportIds := measuresrtrv.GetAirports()
+	for _, element := range airportIds {
+		datas.Aeroports = append(datas.Aeroports, data.Aeroport{Name: element, Id: element})
+	}
+	t := template.Must(template.ParseFiles("tmpl/pageAcceuil.html"))
+	fmt.Println(t.Execute(w, datas))
+}
+
+func airportHandeler(w http.ResponseWriter, r *http.Request) {
+	airport := strings.Replace(r.URL.Path, "/airport/", "", 1)
+	datas := data.AirportData{Airportname: airport}
+	t := template.Must(template.ParseFiles("tmpl/aeroportDetails.html"))
+
+	fmt.Println(t.Execute(w, datas))
 }
 
 func main() {
+	redis.Connect()
+
+	http.Handle("/tmpl/", http.StripPrefix("/tmpl/", http.FileServer(http.Dir("tmpl"))))
 	http.HandleFunc("/", templateHandeler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/airport/", airportHandeler)
+	http.ListenAndServe(":8085", nil)
+	defer redis.CloseConnection()
 }
---
+++ internal/data/datas.go
@@ -0,0 +1,13 @@
+package data
+
+type Aeroport struct {
+	Name string
+	Id   string
+}
+type Datas struct {
+	Aeroports []Aeroport
+}
+
+type AirportData struct {
+	Airportname string
+}
