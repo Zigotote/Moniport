72bde41f6d0a59d1b48a04fb6e6c57791a05ca7f
Zigotote <oce88b@gmail.com>
2019-10-25 19:05:24 +0200

    Avancement bash

--- run.sh
+++ run.sh
@@ -1,28 +1,56 @@
-cd $GOPATH/src/Moniport/cmd/recepteur
+repo=$GOPATH/src/Moniport
 
-# Lancement du recepteur
+# Build tous les fichier Go du dossier internal/
+for dir in $(find internal/* -type d)
+do
+    find $dir/* -type d > /tmp/find.txt
+    if [ ! -s /tmp/find.txt ]
+    then
+        echo Build du package $dir 
+        cd $repo/$dir
+        go build
+        cd $repo/
+    fi
+    if [ ! $? -eq 0 ]
+    then
+        echo Erreur lors du build du dossier $dir
+        exit 1
+    fi
+done
 
-go build Moniport/cmd/recepteur
+# Lancement des programmes du dossier cmd/
+cd $repo/cmd
+
+# Récepteur
+
+cd recepteur
+go build 
 if [ ! $? -eq 0 ]
 then
     echo "Erreur lors du build du dossier recepteur"
+    exit 1
+else
+    ./recepteur &
+    echo Lancement du récepteur sur le processus $!
 fi
-./recepteur &
-echo Lancement du récepteur sur le processus $!
+
 
 # Lancement des capteurs
 
 cd ../sensor
 go build Moniport/cmd/sensor
+cd ..
 
 if [ ! $? -eq 0 ]
 then
     echo "Erreur lors du build du dossier sensor"
+    exit 1
+else
+    for config in config-files/*.json 
+    do
+        ./sensor/sensor -config $GOPATH/src/Moniport/cmd/$config &
+        echo Lancement du capteur configuré dans le fichier $config : processus $!
+    done
 fi
 
-cd ..
-for config in config-files/*.json 
-do
-    ./sensor/sensor -config $GOPATH/src/Moniport/cmd/$config &
-    echo Lancement du capteur configuré dans le fichier $config : processus $!
-done
+
