a7eb420938ce75c7a65380e951fa05e14b27d7d8
Simon Le Bars <simon.lebars.22290@gmail.com>
2019-10-27 01:19:36 +0200

    Les capteurs renvois une valeur basée sur une variation de la valeur précédente

--- cmd/sensor/publisher.go
+++ cmd/sensor/publisher.go
@@ -23,10 +23,15 @@ func main() {
 	mqttAdress := "tcp://" + s1.MqttAdress + ":" + strconv.Itoa(s1.MqttPort)
 	c1 := mqtt.Connect(mqttAdress, s1.IdAirport+":"+strconv.Itoa(s1.Id))
 
+	//Valeur de la mesure précédente, utilisé ici pour simuler des variations au lieu de valeurs totalement aléatoires
+	var previous_measure_value float64 = -1
+
 	for range time.Tick(10 * time.Second) {
 		fmt.Printf("Envoi message...")
-		out, err := json.Marshal(s1.GenerateMessage(time.Now()))
+		measure := s1.GenerateMessage(time.Now(), previous_measure_value)
+		out, err := json.Marshal(measure)
 		errorHandler.CheckError(err)
+		previous_measure_value = measure.Value
 		c1.Publish("airport_measures", s1.MqttQos, false, out)
 	}
 }
